d44d21ba4cfab75e84ca4fd8e10a3efe50b1bbfb
unknown <jkim664@gmail.com>
2021-04-21 16:23:24 -0700

    still need to work on making a proper list to get user data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import AddUser from './components/Users/AddUser';
-
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   return (
     <div>
-      <AddUser/>
+      <AddUser />
+      <UsersList users={[]}/>
     </div>
   );
 }
