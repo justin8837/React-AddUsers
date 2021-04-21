ca7ed996eeaa154bbad3cab2fe4ff68fd72dc7e5
unknown <jkim664@gmail.com>
2021-04-21 15:56:45 -0700

    added some buttons and css

---
+++ src/components/UI/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Button.module.css';
+
+const Button = props => {
+    return <button className = {classes.button} type= {props.type || 'button'}></button>
+}
+
+export default Button;
